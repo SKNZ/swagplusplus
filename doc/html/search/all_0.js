95cf40b1addd76c0c0f7903565998e048cc4d471
loick111 <loick111@gmail.com>
2014-12-08 00:10:05 +0100

    update Doxyfile

--- doc/html/search/all_0.js
+++ doc/html/search/all_0.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['addafter',['addAfter',['../classnsSdD_1_1CList_1_1CNode.html#ab18a47c21c385ce5072df88c831c523e',1,'nsSdD::CList::CNode']]],
   ['addbefore',['addBefore',['../classnsSdD_1_1CList_1_1CNode.html#a5c11b581e59233a828fb5618b36f1457',1,'nsSdD::CList::CNode']]],
-  ['assign',['assign',['../classnsSdD_1_1CList.html#a27b48887bd136140f3f79192b79f8897',1,'nsSdD::CList']]]
+  ['assign',['assign',['../classnsSdD_1_1CList.html#a27b48887bd136140f3f79192b79f8897',1,'nsSdD::CList::assign(unsigned n, const T &amp;val) noexcept'],['../classnsSdD_1_1CList.html#a11db16b0200f115876884342e232d8b4',1,'nsSdD::CList::assign(InputIterator begin, InputIterator last) noexcept']]]
 ];
